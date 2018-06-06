3ed9d09f4ccc7147fe11e667d30d844f619759b1
Benjamin Konicek <ben.konicek@gmail.com>
2018-06-05 21:55:04 -0400

    added dev group and additional users

--- global/iam.tf
+++ global/iam.tf
@@ -11,6 +11,25 @@ resource "aws_iam_user_policy_attachment" "s3_kunal" {
   policy_arn = "${var.s3_access_policy}"
 }
 
+resource "aws_iam_user" "matt" {
+  name = "Matt"
+}
+
+resource "aws_iam_user" "donna" {
+  name = "Donna"
+}
+
+resource "aws_iam_group_membership" "dev_members" {
+  name = "Dev Users"
+
+  users = [
+    "${aws_iam_user.donna.name}",
+    "${aws_iam_user.matt.name}",
+  ]
+
+  group = "${aws_iam_group.dev.name}"
+}
+
 resource "aws_iam_account_password_policy" "pw_policy" {
   allow_users_to_change_password = "${var.allow_user_change}"
   minimum_password_length        = "${var.min_pw_length}"
@@ -20,4 +39,13 @@ resource "aws_iam_account_password_policy" "pw_policy" {
   max_password_age               = "${var.max_pw_age}"
   hard_expiry                    = "${var.expiry}"
   password_reuse_prevention      = "${var.password_reuse}"
+}
+
+resource "aws_iam_group" "dev" {
+  name = "Dev"
+}
+
+resource "aws_iam_group_policy_attachment" "s3_dev_group" {
+  policy_arn = "${var.s3_access_policy}"
+  group      = "${aws_iam_group.dev.name}"
 }
